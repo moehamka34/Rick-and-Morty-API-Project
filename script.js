aba981b16f9a5cc3b275d09ead4ea6038a64f1f6
Mohamed Hamka <mohamedhamka@Mohameds-MBP-2.attlocal.net>
2022-05-11 20:07:39 -0400

    finished with HTML and CSS

--- script.js
+++
@@ -1,9 +0,0 @@
-// the fetch method allows me to recieve the API from the site.
-fetch("https://rickandmortyapi.com/api/character")
-//the 'then' method allows me to connect the data to my site.
-    .then(response => response.json())
-    .then(data => MakeGen(data.results))
-// running the function to see if it's connected to the DOM
-    function MakeGen(run){
-        console.log(run)}
-        
